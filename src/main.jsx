7db957cd015a22c3e0264e12b93bb1257d9dc414
tiffany <tiffany871205@gmail.com>
2026-01-24 20:43:40 +0800

    feat: quill及main.jsx修改、doc:新增變數設定的圖片到資料夾內

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import App from "./App.jsx";
 
 import "./assets/all.scss";
 
+import "quill/dist/quill.snow.css"; //先安裝quill -> bash: npm install quill
+import * as bootstrap from "bootstrap";
+import Quill from "quill";
+
+/* 所有頁面:popover跳出式視窗初始化 */
+document.addEventListener("DOMContentLoaded", () => {
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+  [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />

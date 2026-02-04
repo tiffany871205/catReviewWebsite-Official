47dece18bbbe07d56e23f20c4035adec5a6b113d
tiffany <tiffany871205@gmail.com>
2026-02-04 15:03:33 +0800

    feat:刪除測試功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,6 @@
 import { Outlet, Link } from "react-router";
 import axios from "axios";
 
-function signUp() {
-  axios
-    .post("http://localhost:3000/signup", {
-      email: "test123@gmail.com",
-      password: "password123",
-      username: "testuser",
-    })
-    .then((response) => {
-      console.log("註冊成功:", response.data);
-      alert("註冊成功！");
-    })
-    .catch((error) => {
-      console.error("註冊失敗:", error.response?.data || error.message);
-      alert("註冊失敗");
-    });
-}
-
 function App() {
   return (
     <>
@@ -29,7 +12,6 @@ function App() {
       </nav>
       <hr />
       <Outlet />
-      <button onClick={signUp}>註冊</button>
       <h1>這個是外層</h1>
     </>
   );

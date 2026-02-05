61a4229079a2d7835a33ddb237253a092470a424
tiffany <tiffany871205@gmail.com>
2026-02-05 18:10:44 +0800

    feat:刪除主頁nav(暫時保留註冊)

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,6 @@ function App() {
     <>
       <h1>這個是外層</h1>
       <Header />
-      <nav>
-        <Link to="/index">首頁</Link> |<Link to="/food">食品</Link> |
-        <Link to="/knowledge">知識</Link> |<Link to="/contrib">投稿</Link> |
-        <Link to="/member">會員</Link>
-      </nav>
       <button onClick={signUp}>註冊</button>
       <hr />
       <Outlet />

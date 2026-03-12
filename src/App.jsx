9723c60d76da2fe6398939b0ec8a8412367f1000
tiffany <tiffany871205@gmail.com>
2026-03-12 19:59:36 +0800

    feat: 優化路由首頁行為與切頁回到頂端

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import signUp from "./utils/signUp.js";
 import Header from "./components/common/Header.jsx";
 import Footer from "./components/common/Footer.jsx";
+import ScrollToTop from "./components/common/ScrollToTop.jsx";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       {/* <button onClick={signUp}>註冊</button>
       <hr /> */}
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -3,12 +3,12 @@ import App from "../App.jsx";
 import Index from "../pages/Index.jsx";
 import Food from "../pages/Food.jsx";
 import Contrib from "../pages/Contrib.jsx";
-import Member from "../pages/member.jsx";
+import Member from "../pages/Member.jsx";
 import Account from "../pages/member/Account.jsx";
 import Record from "../pages/member/Record.jsx";
 import SignUp from "../components/common/SignUp.jsx";
-import Knowledge from "../pages/knowledge.jsx";
-import KnowledgeArticle from "../pages/article.jsx";
+import Knowledge from "../pages/Knowledge.jsx";
+import KnowledgeArticle from "../pages/Article.jsx";
 import Comment from "../pages/member/Comment.jsx";
 import Favorite from "../pages/member/Favorite.jsx";
 import Contribution from "../pages/member/Contribution.jsx";
@@ -18,6 +18,10 @@ const routes = [
     path: "/",
     element: <App />,
     children: [
+      {
+        index: true,
+        element: <Index />,
+      },
       {
         path: "index",
         element: <Index />,
